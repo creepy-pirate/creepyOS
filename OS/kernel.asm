0277b85c9c85d68f7b41f37c49453af2b38f9ee3
creepy-pirate <dv72276@gmail.com>
2021-06-19 01:12:38 +0530

    Creating the kernel.asm Adding explainations

--- OS/kernel.asm
+++ OS/kernel.asm
@@ -6,8 +6,11 @@ section         .text
         dd      - (0x1BADB002+0x00)
         
 global start
-extern kmain            ; (kernel.c)
+extern kmain            ;function will located in kernel.c
 start:
         cli             ;clear interrupts
         call kmain      ;sends the processor to continue the execution from the kmain funtion in the C code
-        hlt             ;
+        hlt             ; halts the CPU
+
+
+                        ; 0x1BADB002 this the number which indicates it
